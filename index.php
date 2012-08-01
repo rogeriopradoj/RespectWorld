ffd7c890923d5ff12c74c8989859ceeca0437d5d
Rogerio Prado de Jesus <c066770@mail.caixa>
2012-08-01 15:06:29 -0300

    First routes in index.php
    Signed-off-by: Rogerio Prado de Jesus <c066770@mail.caixa>

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
 <?php
-    
+require 'vendor/autoload.php';
+
+use Respect\Rest\Router;
+
+$r3 = new Router;
+
+$r3->get('/', function() {
+    return 'Hello World';
+});
+
+$r3->get('/hello', function() {
+    return 'Hello from Path';
+});
+
+$r3->get('/users/*', function($screenName) {
+    echo "User {$screenName}";
+});
+
+$r3->get('/users/*/lists/*', function($user, $list) {
+    return "List {$list} from user {$user}.";
+});
+
+$r3->get('/posts/*/*/*', function($year,$month=null,$day=null) {
+    //list posts, month and day are optional
+});
